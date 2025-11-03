8b7d3c39e5cfc447ea0cb193434f7e7c569d9b66
Kofijoo <joshuaagyekum21@gmail.com>
2025-11-03 02:22:06 +0100

    Fix GitHub Pages: correct homepage URL and rebuild with all 6 presentations

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Instructional_Design_Portfolio">
       <div className="app">
         <Navigation />
         <Routes>
